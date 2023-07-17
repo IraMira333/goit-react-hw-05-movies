f8d75650174f5e91d86bd8ab20ee4372495cdf97
IraMira333 <irinat333@gmail.com>
2023-07-17 23:20:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
 import MoviePage from 'pages/MoviePage';
+import HomePage from 'pages/HomePage';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<div>Home</div>} />
+        <Route index element={<HomePage />} />
         <Route path="movies" element={<MoviePage />} />
         <Route path="movies/:movieId" element={<div>MovieDetails</div>}>
           <Route path="cast" element={<div>Cast</div>} />
