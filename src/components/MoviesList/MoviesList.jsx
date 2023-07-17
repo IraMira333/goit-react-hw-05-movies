bb4dd4fc67c6f91a5cdb2c6b5bbb8b9487ba5b0f
IraMira333 <irinat333@gmail.com>
2023-07-17 23:20:31 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,5 +1,7 @@
-const MoviesList = () => {
-  return <div>List</div>;
+const MoviesList = movies => {
+  return movies.map(movie => {
+    return <li key={movie.id}>{movie.title}</li>;
+  });
 };
 
 export default MoviesList;
