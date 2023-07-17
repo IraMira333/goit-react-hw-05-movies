53e176baf2ff7256e434623786373c3937da54d2
IraMira333 <irinat333@gmail.com>
2023-07-17 23:24:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ export const App = () => {
           <Route path="cast" element={<div>Cast</div>} />
           <Route path="reviews" element={<div>Reviews</div>} />
         </Route>
-        <Route path="*" element={<div>Home</div>} />
+        <Route path="*" element={<HomePage />} />
       </Route>
     </Routes>
   );
