f928f8ed23fdaa86f745a00d2cbc881e5150c7ac
IraMira333 <irinat333@gmail.com>
2023-07-18 00:56:30 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
 import MoviePage from 'pages/MoviePage';
 import HomePage from 'pages/HomePage';
+import MovieDetails from 'pages/MovieDetailsPage';
 
 export const App = () => {
   return (
@@ -9,7 +10,7 @@ export const App = () => {
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
         <Route path="movies" element={<MoviePage />} />
-        <Route path="movies/:movieId" element={<div>MovieDetails</div>}>
+        <Route path="movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<div>Cast</div>} />
           <Route path="reviews" element={<div>Reviews</div>} />
         </Route>
