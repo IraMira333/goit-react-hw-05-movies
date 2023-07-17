7fb9d1e361c8b8f172f7f6d5ca62fe43a8bc02d4
IraMira333 <irinat333@gmail.com>
2023-07-17 16:46:56 +0300

    Create SharedLayout.jsx

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,17 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
+const SharedLayout = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+        </nav>
+      </header>
+      <Outlet />
+    </>
+  );
+};
+
+export default SharedLayout;
