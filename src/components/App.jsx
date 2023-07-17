e0189f709fccadea75ccc974a219b1181862530d
IraMira333 <irinat333@gmail.com>
2023-07-17 16:46:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from './SharedLayout/SharedLayout';
+import MoviePage from 'pages/MoviePage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<div>Home</div>} />
+        <Route path="movies" element={<MoviePage />} />
+        <Route path="movies/:movieId" element={<div>MovieDetails</div>}>
+          <Route path="cast" element={<div>Cast</div>} />
+          <Route path="reviews" element={<div>Reviews</div>} />
+        </Route>
+        <Route path="*" element={<div>Home</div>} />
+      </Route>
+    </Routes>
   );
 };
