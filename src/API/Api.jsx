849400f6492f3a5bb970eeebdd0a1091bfc0569b
IraMira333 <irinat333@gmail.com>
2023-07-17 23:20:23 +0300

    Update Api.jsx

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const URL = 'https://api.themoviedb.org/3/';
+const URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'ba8df02c5548f4248e5d9d0723885591';
 
 export const getApi = async (endPoint, dataQuery) => {
-  const response = await axios.get(
+  const { data } = await axios.get(
     `${URL}${endPoint}?api_key=${API_KEY}${
       dataQuery ? `&query=${dataQuery}` : ''
     }&language=en-US`
   );
-  console.log(response);
-  return response;
+  // console.log(data);
+  return data;
 };
 
 export default getApi;
