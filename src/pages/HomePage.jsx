e0745d0b96b3e92e2a03984236fb83091c79bcb1
IraMira333 <irinat333@gmail.com>
2023-07-18 00:56:35 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,10 @@
 import getApi from 'API/Api';
 import Spiner from 'components/Spiner/Spiner';
 import TrendingMovies from 'components/TrendingMovies/TrendingMovies';
+import toastConfig from 'components/toastConfig';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-const toastConfig = {
-  position: 'top-center',
-  autoClose: 5000,
-  hideProgressBar: false,
-  closeOnClick: true,
-  pauseOnHover: true,
-  draggable: true,
-  progress: undefined,
-  theme: 'colored',
-};
 
 const HomePage = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
