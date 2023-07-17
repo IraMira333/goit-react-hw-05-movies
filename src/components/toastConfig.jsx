79137e0d358f16ba7dc77de37046c1fcfe15cab5
IraMira333 <irinat333@gmail.com>
2023-07-18 00:56:32 +0300

    Create toastConfig.jsx

---
+++ src/components/toastConfig.jsx
@@ -0,0 +1,12 @@
+const toastConfig = {
+  position: 'top-center',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'colored',
+};
+
+export default toastConfig;
