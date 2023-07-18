590e0d9f85d0979a08c76e93a113c0e3aa41c965
IraMira333 <irinat333@gmail.com>
2023-07-18 23:59:47 +0300

    Update Reviews.jsx

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,55 @@
+import getApi from 'API/Api';
+import Spiner from 'components/Spiner/Spiner';
+import toastConfig from 'components/toastConfig';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const Reviews = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [reviews, setReviews] = useState([]);
+  const [err, setErr] = useState(null);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    async function getMovieReviews() {
+      try {
+        setIsLoading(true);
+
+        const movieReviews = await getApi(`/movie/${movieId}/reviews`);
+        if (!movieReviews) {
+          return;
+        }
+        setReviews(movieReviews.results);
+
+        console.log(movieReviews.results);
+      } catch (error) {
+        console.log(err);
+        setErr(error.message);
+
+        toast.error(error.message, toastConfig);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getMovieReviews();
+  }, [err, movieId]);
+
+  return (
+    <>
+      {isLoading && <Spiner />}
+      {reviews.length === 0 && <p>We don't have any reviews for this movie.</p>}
+      <ul>
+        {reviews?.map(({ id, author, content }) => {
+          return (
+            <li key={id}>
+              <p>Author: {author}</p>
+              <p>{content}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+export default Reviews;
