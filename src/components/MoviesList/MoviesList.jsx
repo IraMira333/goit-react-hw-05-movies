8ee7f2eb23d51567c83941b1c2458f9cf3d0a317
IraMira333 <irinat333@gmail.com>
2023-07-18 19:29:02 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,7 +1,24 @@
-const MoviesList = movies => {
-  return movies.map(movie => {
-    return <li key={movie.id}>{movie.title}</li>;
-  });
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const MoviesList = ({ movies }) => {
+  return (
+    <ul>
+      {movies.map(movie => {
+        return (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>
+              <p>{movie.title}</p>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+MoviesList.propTypes = {
+  movies: PropTypes.array.isRequired,
 };
 
 export default MoviesList;
