d6ff78a12323d5c83432934be767b669479aebe7
IraMira333 <irinat333@gmail.com>
2023-07-19 00:10:44 +0300

    Update TrendingMovies.jsx

--- src/components/TrendingMovies/TrendingMovies.jsx
+++ src/components/TrendingMovies/TrendingMovies.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const TrendingMovies = ({ trendingMovies }) => {
@@ -14,6 +15,10 @@ const TrendingMovies = ({ trendingMovies }) => {
       })}
     </ul>
   );
+};
+
+TrendingMovies.propTypes = {
+  trendingMovies: PropTypes.array.isRequired,
 };
 
 export default TrendingMovies;
