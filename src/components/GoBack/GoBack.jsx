dc0db9c2f16f19fb9ebb62f80a8af7a334504fd2
IraMira333 <irinat333@gmail.com>
2023-07-19 23:07:52 +0300

    Update GoBack.jsx

--- src/components/GoBack/GoBack.jsx
+++ src/components/GoBack/GoBack.jsx
@@ -2,9 +2,9 @@ import { HiArrowLeft } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 import css from './GoBack.module.css';
 
-const Goback = ({ location }) => {
+const Goback = ({ backLinkLocationRef }) => {
   return (
-    <Link to={location.state.from} className={css.goback}>
+    <Link to={backLinkLocationRef} className={css.goback}>
       <HiArrowLeft /> Go back
     </Link>
   );
