c7130ea539b695ac149749a97b24fb7b621309c4
IraMira333 <irinat333@gmail.com>
2023-07-19 17:42:35 +0300

    Create GoBack.jsx

---
+++ src/components/GoBack/GoBack.jsx
@@ -0,0 +1,13 @@
+import { HiArrowLeft } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+import css from './GoBack.module.css';
+
+const Goback = ({ location }) => {
+  return (
+    <Link to={location.state.from} className={css.goback}>
+      <HiArrowLeft /> Go back
+    </Link>
+  );
+};
+
+export default Goback;
