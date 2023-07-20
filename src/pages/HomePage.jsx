7a3cc991c87813d344852462c091d520ee89a5f2
IraMira333 <irinat333@gmail.com>
2023-07-20 12:26:30 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import getApi from 'API/Api';
 import MoviesList from 'components/MoviesList/MoviesList';
-import Spiner from 'components/Spiner/Spiner';
 import toastConfig from 'components/toastConfig';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
@@ -9,13 +8,10 @@ import 'react-toastify/dist/ReactToastify.css';
 const HomePage = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
   const [err, setErr] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getTrendingMovie() {
       try {
-        setIsLoading(true);
-
         const movie = await getApi('/trending/movie/day');
         const moviesTitleList = movie.results.map(({ title, id }) => {
           return { title, id };
@@ -26,19 +22,17 @@ const HomePage = () => {
         console.log(err);
         setErr(error.message);
         toast.error(error.message, toastConfig);
-      } finally {
-        setIsLoading(false);
       }
     }
     getTrendingMovie();
   }, [err]);
 
   return (
-    <main>
+    <>
       <h1>Trending Today</h1>
-      {isLoading && <Spiner />}
+
       <MoviesList movies={trendingMovies} />
-    </main>
+    </>
   );
 };
 
