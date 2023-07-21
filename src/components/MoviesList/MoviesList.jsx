c46bf6ea13a56ff4556a22726d063f25080689ce
IraMira333 <irinat333@gmail.com>
2023-07-21 13:57:04 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,21 +1,26 @@
 import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import images from '../images/film-popkorn-700x466-1-min.jpg';
+import css from './MoviesList.module.css';
 
 const MoviesList = ({ movies }) => {
   const location = useLocation();
 
   return (
-    <ul>
+    <ul className={css.trendingBox}>
       {movies.map(({ id, tagline, title, poster_path }) => {
         const poster = poster_path
           ? `https://image.tmdb.org/t/p/w400${poster_path}`
           : images;
         return (
-          <li key={id}>
-            <Link to={`/movies/${id}`} state={{ from: location }}>
+          <li key={id} className={css.trendingItem}>
+            <Link
+              className={css.link}
+              to={`/movies/${id}`}
+              state={{ from: location }}
+            >
               <img src={poster} alt={tagline} width={250} />
-              <p>{title}</p>
+              <p className={css.title}>{title}</p>
             </Link>
           </li>
         );
