a0c78d3ec78a507f6f184e55d94dcaaf22e62d77
IraMira333 <irinat333@gmail.com>
2023-07-21 13:56:55 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import avatar from '../images/icon-user-640x640-min.png';
+import css from './Cast.module.css';
 
 const Cast = () => {
   const [cast, setCast] = useState([]);
@@ -32,14 +33,14 @@ const Cast = () => {
   return (
     <>
       {!cast && <p>We haven't found any cast information</p>}
-      <ul>
+      <ul className={css.castBox}>
         {cast?.map(({ profile_path, id, name, character }) => {
           const foto = profile_path
             ? `https://image.tmdb.org/t/p/w400${profile_path}`
             : avatar;
           return (
-            <li key={id}>
-              <img src={foto} alt={name} width={120} />
+            <li className={css.castItem} key={id}>
+              <img className={css.photo} src={foto} alt={name} width={120} />
               <p>{name}</p>
               <p>Character: {character}</p>
             </li>
