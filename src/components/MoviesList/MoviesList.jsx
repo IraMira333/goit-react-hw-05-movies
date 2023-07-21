e735e991c8d44a2c71fdc4c9a4bac3c3d1c63f26
IraMira333 <irinat333@gmail.com>
2023-07-21 12:59:29 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,15 +1,21 @@
 import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import images from '../images/film-popkorn-700x466-1-min.jpg';
 
 const MoviesList = ({ movies }) => {
   const location = useLocation();
+
   return (
     <ul>
-      {movies.map(movie => {
+      {movies.map(({ id, tagline, title, poster_path }) => {
+        const poster = poster_path
+          ? `https://image.tmdb.org/t/p/w400${poster_path}`
+          : images;
         return (
-          <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`} state={{ from: location }}>
-              <p>{movie.title}</p>
+          <li key={id}>
+            <Link to={`/movies/${id}`} state={{ from: location }}>
+              <img src={poster} alt={tagline} width={250} />
+              <p>{title}</p>
             </Link>
           </li>
         );
